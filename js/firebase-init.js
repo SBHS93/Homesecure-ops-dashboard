15e07df75c505182c9c3913e245d5ff5e79044ac
SBHS93 <homesecuresam93@yahoo.com>
2026-09-07 14:02:06 +0100

    Add files via upload

---
+++ js/firebase-init.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjA-LTSvWiJqVjMh6SLMI3AkumPJgl2ro",
+  authDomain: "home-ops-dashboard.firebaseapp.com",
+  projectId: "home-ops-dashboard",
+  storageBucket: "home-ops-dashboard.firebasestorage.app",
+  messagingSenderId: "668957172690",
+  appId: "1:668957172690:web:574a59d4e106e7537fde0b",
+  measurementId: "G-ZE2TSRNMMD"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
